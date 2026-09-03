cdb6817137e9ff3c59c51e45a89c884d09de51fe
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-03 16:07:19 +0530

    Create ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (~i && s[i] == ' ') {
+            --i;
+        }
+        int j = i;
+        while (~j && s[j] != ' ') {
+            --j;
+        }
+ 
