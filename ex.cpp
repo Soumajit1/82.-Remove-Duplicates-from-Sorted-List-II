746da9309e6c995140ee5e07f0682db05bbee2fb
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-03 16:07:43 +0530

    Update ex.cpp

--- ex.cpp
+++ ex.cpp
@@ -4,9 +4,17 @@ public:
         int i = s.size() - 1;
         while (~i && s[i] == ' ') {
             --i;
+        }class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (~i && s[i] == ' ') {
+            --i;
         }
         int j = i;
         while (~j && s[j] != ' ') {
             --j;
         }
- 
+        return i - j;
+    }
+};
